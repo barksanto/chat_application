af3d94af6d229dc193d2592029f897d4b74bb8f0
Barkley Santo <barkley.santo@gmail.com>
2021-07-02 13:38:44 -0500

    added ChatEngine credentials + project id

--- src/App.js
+++ src/App.js
@@ -1,2 +1,15 @@
 import { ChatEngine } from 'react-chat-engine';
-import './App.css'
+import './App.css';
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="9313184b-ed2c-4f73-8d7b-c7732711f349"
+      userName="barkley"
+      userSecret="123123"
+    />
+  )
+}
+
+export default App;
