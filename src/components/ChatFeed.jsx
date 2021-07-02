510d78d2d79b8478f893343e9cd7495854e0cc1b
Barkley Santo <barkley.santo@gmail.com>
2021-07-02 14:19:26 -0500

    depending on whose message is last, render that. dynamic styling depending on who sent the message

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,6 +1,44 @@
 // version 17 do not need to import React
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+
 const ChatFeed = (props) => {
-  console.log(props);
+  // console.log(props);
+  const { chats, activeChat, userName, messages } = props;
+  const chat = chats && chats[activeChat];
+
+  const renderMessages = () => {
+    const keys = Object.keys(messages); //this takes the keys from our messages 
+
+    return keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1]; //if there are messages find the last message
+      const isMyMessage = userName === message.sender.username;
+
+      return (
+        <div key={`msg+${index}`} style={{ width: '100%' }}>
+          <div className="message-block">
+            {
+              isMyMessage
+                ? <MyMessage />
+                : <TheirMessage />
+            }
+          </div>
+          {/* add some styling depending on whose message it is */}
+          <div className="read-receipts" style={{ marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px' }}>
+
+          </div>
+        </div >
+      )
+    })
+  }
+
+  renderMessages();
+
+
+
+  // renderMessages();
 
   return (
     <div>
